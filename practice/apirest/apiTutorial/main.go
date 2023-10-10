82711647176e0e328f438c2cb0ed376bfbf03d7c
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-10-10 12:55:20 -0300

    feat(main.go)

--- practice/apirest/apiTutorial/main.go
+++ practice/apirest/apiTutorial/main.go
@@ -1,29 +1,49 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	// "errors"
 )
 
 type book struct {
-	ID		 string	`json:id`
-	Title	 string	`json:title`
-	Author   string	`json:author`
-	Quantity int 	`json:quantity`
+	ID       string `json:id`
+	Title    string `json:title`
+	Author   string `json:author`
+	Quantity int    `json:quantity`
 }
 
-var books = []book {
+var books = []book{
 	{ID: "1", Title: "Clean Architecture", Author: "Uncle Bob", Quantity: 8},
 	{ID: "2", Title: "Programing Go Language", Author: "Other", Quantity: 4},
 	{ID: "3", Title: "Clean Code", Author: "Uncle Bob", Quantity: 10},
 }
 
-func getBooks(c *gin.Context){
+func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
-func createBook(c *gin.Context){
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New(("book not found"))
+}
+
+func createBook(c *gin.Context) {
 	var newBook book
 	if err := c.BindJSON(&newBook); err != nil {
 		return
@@ -32,11 +52,12 @@ func createBook(c *gin.Context){
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
-func main(){
+func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
 
-//curl https://t4nkvl-8080.csb.app/books --include --header "Content-Type: application/json" -d @body.json --request "POST"      
+//curl https://t4nkvl-8080.csb.app/books --include --header "Content-Type: application/json" -d @body.json --request "POST"
