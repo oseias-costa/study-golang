4b84d1435970fbfb75927cc282db15759c70f303
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-10-10 16:56:11 -0300

    feat(checkoutBook): create checkout book function

--- practice/apirest/apiTutorial/main.go
+++ practice/apirest/apiTutorial/main.go
@@ -43,6 +43,28 @@ func getBookById(id string) (*book, error) {
 	return nil, errors.New(("book not found"))
 }
 
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missin id query parameter"})
+		return
+	}
+	book, err := getBookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func createBook(c *gin.Context) {
 	var newBook book
 	if err := c.BindJSON(&newBook); err != nil {
@@ -57,6 +79,7 @@ func main() {
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
+	router.PATH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 }
 
