68e8b123ea58c3b9a64bea6fea039d5768beb1b2
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-10-21 23:44:00 -0300

    feat(go.work): work with multiple projects in folder

---
+++ practice/api-go-file/domain/person/person.go
@@ -0,0 +1,98 @@
+package person
+
+import (
+	"os"
+	"practice/api-go-file/domain"
+)
+
+type Service struct {
+	dbFilePath string
+	people     domain.People
+}
+
+func NewService(dbFilePath string) (Service, error) {
+	//verifica se o arquivo existe
+	_, err := os.Stat(dbFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// se não existir cria arquivo vazio
+			err := createEmptyFile(dbFilePath)
+			if err != nil {
+				return Service{}, err
+			}
+
+			return Service{
+				dbFilePath: dbFilePath,
+				people: domain.People{},
+			}, nil
+		}
+	}
+
+	// se existir crio
+	jsonFile, err := os.Open(dbFilePath)
+	if err != nil {
+		return Service {}, fmt.Error("Error trying to open file that contains all people: %s", err.Error())
+	}
+
+	jsonFileContentByte, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return Service{}, fmt.Errorf("Error trying to read file: %s", err.Error())
+	}
+
+	var allPeople domain.People
+	json.Unmarshal(jsonFileContentByte, &allPeople)
+
+	return Service{
+		dbFilePath: dbFilePath,
+		people: allPeople,
+	}, nil
+}
+
+func createEmptyFile(dbFilePath string) error {
+	var people domain.People = domain.People{
+		People: []domain.Person{},
+	}
+	peopleJSON, err := json.Marshal(people)
+	if err != nil {
+		return fmt.Errorf("Error trying to encode people as JSON?: " %s, err.Error())
+	}
+	err = ioutil.WriteFile(dbFilePath, peopleJSON, 0755)
+	if err != {
+		return fmt.Errorf("Error trying to write to file. Error: %s", err.Error())
+	}
+	return nil
+}
+
+func (s Service) Create(person domain.Person) error {
+	// verfify if exist
+	if s.exist(person){
+		return fmt.Error("Error trying to create person. There is a person with this ID already registered")
+	}
+	// add
+	s.people.People = append(s.people.People, person)
+
+	// save file
+	err := s.saveFile()
+	if err != nil {
+		return fmt.Errorf("error trying save file in create method")
+	}
+
+	return nil
+}
+
+func (s Service) exists(person domain.Person) bool {
+	for _, currentPerson := range s.people.People {
+		if currentPerson.ID == person.ID {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Service) saveFile() error {
+	allPeopleJSON, err := json.Marshal(s.people)
+	if err != nil {
+		return fmt.Errorf("Error trying to encode people as json: %s", err.Error())
+	}
+	return ioutil.WriteFile(s.dbFilePath, allPeopleJSON, 0755)
+}
--- practice/api-go-file/main.go
+++ practice/api-go-file/main.go
@@ -1,9 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"practice/api-go-file/domain"
+	"practice/api-go-file/domain/person"
+)
 
-func main(){
-	http.HandleFunc("/person/", func(w http.ResponseWriter, r *http.Request){
+func main() {
+	personService, err := person.NewService("person.json")
+	if err != nil {
+		fmt.Println("Error trying to create person service")
+		return
+	}
+
+	http.HandleFunc("/person/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+
+			var person domain.Person
+			err := json.NewDecoder(r.Body).Decode(&person)
+			if err != nil {
+				fmt.Printf("Error trying to decode body. Body shold be a json. Error: %s", err.Error())
+				http.Error(w, "Error trying to create person", http.StatusBadRequest)
+				return
+			}
+
+			if person.ID <= 0 {
+				http.Error(w, "Error trying to create person. ID shoud be a positive integer", http.StatusBadRequest)
+			}
+
+			// criar pessoa
+			err = personService.Create(person)
+			if err != nil {
+				fmt.Printf("Error trying crete person: %s", err.Error())
+				http.Error(w, "Error trying to create person", http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusCreated)
+			return
+		}
+
 		http.Error(w, "Not implemented", http.StatusInternalServerError)
 	})
 
